391f8da133e2790c0f24eb1aa2ce0d08b2b8928c
S.K Mohamed javidh <mohamedjavidh2k5@gmail.com>
2026-02-26 19:02:10 +0530

    Modify script for Cookie Clicker automation
    Updated the script to automate cookie clicking on a new website with added functionality.

--- main.py
+++ main.py
@@ -1,17 +1,63 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import  By
+from selenium.webdriver.common.by import By
+import time
+
 chrome_option = webdriver.ChromeOptions()
 chrome_option.add_experimental_option("detach",True)
-
-
 driver = webdriver.Chrome(options=chrome_option)
-driver.get("https://www.python.org")
-
-upcoming_events_names=driver.find_elements(By.CSS_SELECTOR,".event-widget li a")
-upcoming_events_dates=driver.find_elements(By.CSS_SELECTOR,".event-widget time")
-upcoming_events ={}
-for n in range(0,len(upcoming_events_names)):
-    upcoming_events.update({n:{"Time":upcoming_events_dates[n].text,"name":upcoming_events_names[n].text}})
+driver.get("http://ozh.github.io/cookieclicker/")
 
 
-print(upcoming_events)
+
+time.sleep(5)
+select_language = driver.find_element(By.XPATH,'//*[@id="langSelect-EN"]')
+select_language.click()
+
+button = driver.find_element(By.ID,"bigCookie")
+
+end_time = time.time() + 5
+game_timing = time.time() + 60 *5
+
+while True:
+    button.click()
+    cookie = driver.find_element(By.ID, "cookies").text.split()
+    cookie_num = int(cookie[0].strip().replace(",",""))
+    cursor = driver.find_element(By.XPATH, '//*[@id="product0"]')
+    cursor_price = int(driver.find_element(By.ID, "productPrice0").text.strip())
+    grand_ma = driver.find_element(By.XPATH, '//*[@id="product1"]')
+    grand_ma_price = int(driver.find_element(By.ID, "productPrice1").text.strip())
+    if time.time() > end_time:
+        if  cookie_num >= grand_ma_price:
+            grand_ma.click()
+            end_time = time.time() + 5
+        elif cookie_num >= cursor_price:
+            cursor.click()
+            end_time = time.time() + 5
+    if time.time() > game_timing:
+        print(f"Final result :{cookie_num}")
+        driver.quit()
+        break
+
+button = driver.find_element(By.ID,"bigCookie")
+end_time = time.time() + 5
+game_timing = time.time() + 60 *5
+
+while True:
+    button.click()
+    cookie = driver.find_element(By.ID, "cookies").text.split()
+    cookie_num = int(cookie[0].strip().replace(",",""))
+    cursor = driver.find_element(By.XPATH, '//*[@id="product0"]')
+    cursor_price = int(driver.find_element(By.ID, "productPrice0").text.strip())
+    grand_ma = driver.find_element(By.XPATH, '//*[@id="product1"]')
+    grand_ma_price = int(driver.find_element(By.ID, "productPrice1").text.strip())
+    if time.time() > end_time:
+        if  cookie_num >= grand_ma_price:
+            grand_ma.click()
+            end_time = time.time() + 5
+        elif cookie_num >= cursor_price:
+            cursor.click()
+            end_time = time.time() + 5
+    if time.time() > game_timing:
+        print(f"Final result :{cookie_num}")
+        driver.quit()
+        break
